797e70b6991188e5d7ecb3a306f97f25e429c845
majok31 <majoktot70@gmail.com>
2024-09-08 03:18:16 -0700

    updates

--- Makefile
+++ Makefile
@@ -1,8 +1,18 @@
-build:
-	gcc -Wall -Werror -Wextra -pedantic ./src/*.c -lm -o maze `sdl2-config --cflags` `sdl2-config --libs`;
+CC = gcc
+CFLAGS = -I/usr/include/SDL2 -D_REENTRANT
+LDFLAGS = -L/usr/lib -lSDL2 -lSDL2_image -lm
+SRC = $(wildcard src/*.c)
+OBJ = $(SRC:.c=.o)
+TARGET = maze_game
 
-run:
-	./maze;
+all: $(TARGET)
+
+$(TARGET): $(OBJ)
+	$(CC) -o $@ $^ $(LDFLAGS)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
 
 clean:
-	rm maze;
+	rm -f $(OBJ) $(TARGET)
+
--- tests/test_sdl.c
+++ tests/test_sdl.c
@@ -1,21 +1,25 @@
 #include <SDL2/SDL.h>
+#include <stdio.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         printf("SDL initialization failed: %s\n", SDL_GetError());
-	return 1;
-    }
-
-    SDL_Window* window = SDL_CreateWindow("SDL2 Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
-    if (!window) {
-        printf("Window creation failed: %s\n", SDL_GetError());
         return 1;
     }
 
-    SDL_Delay(3000);
+    SDL_Window *window = SDL_CreateWindow(
+        "SDL2 Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+
+    if (!window) {
+        printf("Window creation failed: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Delay(3000); // Keep the window open for 3 seconds
 
     SDL_DestroyWindow(window);
     SDL_Quit();
-
     return 0;
 }
+
